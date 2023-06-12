2bf0f3ce349d8adbc8123b9d0a1590dc45623a1e
ryan2376 <ryannesh2376@gmail.com>
2023-06-12 13:57:56 +0300

    1st

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  });
