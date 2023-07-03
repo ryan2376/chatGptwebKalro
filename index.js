f87e417acc31fde58ac2e31efb6f07c38f4aa2b1
ryan2376 <ryannesh2376@gmail.com>
2023-07-03 11:02:32 +0300

    tyt

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Initialize Swiper for projects section
 document.addEventListener("DOMContentLoaded", function () {
     var swiper = new Swiper('.swiper-container', {
       slidesPerView: 1,
@@ -7,3 +19,15 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     });
   });
+
+// Image grid hover effect
+const imageGridItems = document.querySelectorAll('.image-grid img');
+imageGridItems.forEach(item => {
+  item.addEventListener('mouseover', function () {
+    this.style.transform = 'scale(1.1)';
+  });
+
+  item.addEventListener('mouseout', function () {
+    this.style.transform = 'scale(1)';
+  });
+});
